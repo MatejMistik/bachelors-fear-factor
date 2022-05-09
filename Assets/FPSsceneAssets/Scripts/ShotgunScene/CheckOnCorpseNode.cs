d87cbb1f16e79f782ce2a0884d3be86d67635cc8
MatejMistik <xmisti00@vutbr.cz>
2022-05-09 16:27:31 +0200

    Insert nodes to tree and test.

--- Assets/FPSsceneAssets/Scripts/ShotgunScene/CheckOnCorpseNode.cs
+++ Assets/FPSsceneAssets/Scripts/ShotgunScene/CheckOnCorpseNode.cs
@@ -10,7 +10,16 @@ public class CheckOnCorpseNode : Node
 
     public override NodeState Evaluate()
     {
+        float distance = Vector3.Distance(alliesAround.enemiesToBeChecked[0].transform.position, agent.transform.position);
+        if(distance < 0.2f)
+        {
+            AiCorpseCheck aiCorpse = alliesAround.enemiesToBeChecked[0].GetComponent<AiCorpseCheck>();
+            aiCorpse.corpseWasChecked = true;
+            return NodeState.SUCCESS;
+        }
         agent.SetDestination(alliesAround.enemiesToBeChecked[0].transform.position);
         return NodeState.RUNNING;
+
+        
     }
 }
